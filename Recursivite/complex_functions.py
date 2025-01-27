ef9fbd0f825ff912d19f1700b4c8623e15d798af
davemambote <davemambote@penguin>
2025-01-28 00:36:47 +0100

    Fonctions complexes exo1

---
+++ Recursivite/complex_functions.py
@@ -0,0 +1,10 @@
+#Exo 1
+
+"""
+Tour de Hanoï
+Écrivez une fonction récursive pour résoudre le problème des Tours de Hanoï. Déplacez n disques d’un piquet de départ à un piquet de destination en utilisant un piquet intermédiaire, tout en respectant les règles suivantes :
+
+- Un seul disque peut être déplacé à la fois.
+- Aucun disque ne peut être placé sur un disque plus petit.
+"""
+
